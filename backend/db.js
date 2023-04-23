bcbd35436b1cb21dd8691e564e2995dc38a75452
atishay09 <jainatishay09sep@gmail.com>
2023-04-23 23:37:54 +0530

    changes

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb+srv://amitpayasi65:amitpayasi65@cluster0.lpcmffg.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://amitpayasi:amitpayasi@cluster0.6ghmohh.mongodb.net/test"
 
 // ?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false
 
